a7f250f58aaa8c944870508c9ba2a92d52060c13
YossufAHem <45273413+YossufAHem@users.noreply.github.com>
2018-11-23 03:46:28 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,7 +3,7 @@ const client = new Discord.Client();
 const fs = require('fs');
 const moment = require('moment');
 const jimp = require('jimp');
-const Canvas = require('canvas-prebuilt');
+const Canvas = require('canvas');
 const prefix = '-'
 
 client.on('ready', () => {
